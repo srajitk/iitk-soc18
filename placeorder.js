7faa133c471f30b9e8bf610a3cd7bbcd0096b4c2
srajitk <34921411+srajitk@users.noreply.github.com>
2018-06-23 22:42:25 +0530

    Add files via upload

---
+++ placeorder.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+	$("#place").click(function(){
+		var cat = $("input[name='xxx']").val();
+		if (cat=="fruits")
+			var item = $("select[name='frfood']").val();
+		else 
+			var item = $("select[name='vefood']").val();
+		var aQ = $("input[name='a']").val();
+		var bQ = $("input[name='b']").val();
+		var cQ = $("input[name='c']").val();
+		var paise = $("input[name='cost']").val();
+		var doh = $("input[name='doharvest']").val();
+		var dod = $("input[name='dodeliv']").val();
+		
+		
+		
+		var order = {
+			food: item,
+			category : cat,
+			a : aQ,
+			b : bQ,
+			c : cQ,
+			cost : paise,
+			harvest : doh,
+			deliver : dod
+		};
+		
+		$.ajax({
+			type: "POST",
+			dataType: "json",
+			data: order,
+			url: "placeOrder.php",
+			success: function(data){
+				alert(data);
+			},
+			error: function(data) {
+				alert("something went wrong");
+				alert(JSON.stringify(data));
+			}
+		});
+	
+	});
+});
